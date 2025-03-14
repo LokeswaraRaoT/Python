46f442b51a3e685fccca7cd85ac0451536f529a3
haritha <thalariharitha06@gmail.com>
2025-03-14 11:11:11 +0530

    ifcommit

---
+++ CONDITIONAL.py
@@ -0,0 +1,9 @@
+#Based on some conditions the particular condition is true it will execute some piece of code, 
+# if it is not true then it will go to another piece of code. 
+#Types: if, if else, if elif else
+#Example3: Find out the given number is even or odd number:
+x = int(input("Enter the number: "))
+if x % 2 == 0:
+        print(f"{x} is the even number")
+else:
+        print(f"{x} is the odd number")
