7bf727f7f87324e575f4e5c18db0b01757f6c91f
haritha <thalariharitha06@gmail.com>
2025-03-14 11:20:44 +0530

    whileloopcommit

---
+++ WHILELOOP.py
@@ -0,0 +1,7 @@
+#Print even numbers:					
+x = 1							
+while x < 25: 					
+    if x % 2 == 0:	
+        #if x % 2 != 0: Print odd numbers	   	    
+            print(x, end=",")				        	
+    x += 1			
